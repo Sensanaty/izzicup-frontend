fc174082fe8d40c3b959c494e92e82d321de392e
Luka Salevic <salevic@lu-ka.me>
2020-06-20 23:59:09 +0200

    Finish Navbar styling with logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,25 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
-import '../styles/Navbar.scss';
 
 class Navbar extends Component {
-
     renderLinks = () => {
         if (this.props.loggedIn) {
             return (
                 <>
-                    <li className="nav-item"><NavLink className="nav-link" activeClassName="active" to="/parts">Parts</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" activeClassName="active" to="/logout">Log Out</NavLink></li>
+                    <li className="nav-item"><p className="nav-text">Logged in as {this.props.email}</p></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="/parts">Parts</NavLink></li>
+                    <li className="nav-item logout"><NavLink className="nav-link" to="/logout">Log Out</NavLink></li>
                 </>
             )
-        }
-        return (<li className="nav-item"><NavLink className="nav-link" activeClassName="active" to="/login">Log In</NavLink></li>)
+        } else return (<li className="nav-item login"><NavLink className="nav-link" to="/login">Login</NavLink></li>)
     }
 
     render() {
         return (
             <nav className="navbar">
                 <ul className="navbar-nav">
-                    <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
-                    {this.renderLinks()}
+                    <NavLink className="nav-image-link" to='/'><li className="nav-logo"><img src={process.env.PUBLIC_URL + '/logo.svg'} alt="" aria-hidden='true' className="nav-logo"/></li></NavLink>
+                    { this.renderLinks() }
                 </ul>
             </nav>
         )
