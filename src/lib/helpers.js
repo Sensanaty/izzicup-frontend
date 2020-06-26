501d7e73925b665bb0cf09eea6075519f98daf76
Luka Salevic <salevic@lu-ka.me>
2020-06-26 22:49:20 +0200

    Add jsdoc comment for isDev() function

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,3 +1,7 @@
+/**
+ * Check whether or not the current environment is a dev environment or not
+ * @returns {boolean}
+ */
 export const isDev = () => {
     return !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 }
